0492642a963df1569dcf26f11a044ed2ab941410
kgtkr <contact@kgtkr.net>
2022-02-07 21:51:31 +0900

    update flake

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem
       (system:
-        let pkgs = import nixpkgs { inherit system; }; in
+        let pkgs = nixpkgs.legacyPackages.${system}; in
         rec {
           packages = {
             sqldef-gen-migration = pkgs.callPackage ./. { };
